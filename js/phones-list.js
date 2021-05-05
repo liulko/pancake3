86bfc1148f690b81b46ba9755978199ad85d1b3f
liulko <yurik.lyulko@gmail.com>
2021-05-05 19:06:27 +0300

    last commit. pre-adaptive/responsible.

--- js/phones-list.js
+++ js/phones-list.js
@@ -1,9 +1,13 @@
 let onClickImg = document.getElementById("phone-chevron");
 document.body.addEventListener("click", function (event) {
     if (event.target === onClickImg) {
-    document.getElementById("header__contacts-phones-list-id0").style.opacity = "1";}
+        document.getElementById("header__contacts-phones-list-id0").style.display = "block";
+    }
     else {
-        if (event.target !==  document.getElementById("header__contacts-phones-list-id0") && event.target !==  document.getElementById("header__contacts-phones-list-id1") && event.target !==  document.getElementById("header__contacts-phones-list-id2") && event.target !==  document.getElementById("header__contacts-phones-list-id3") )
-        document.getElementById("header__contacts-phones-list-id0").style.opacity = "0";
+        if (event.target !==  document.getElementById("header__contacts-phones-list-id0") &&
+            event.target !==  document.getElementById("header__contacts-phones-list-id1") &&
+            event.target !==  document.getElementById("header__contacts-phones-list-id2") &&
+            event.target !==  document.getElementById("header__contacts-phones-list-id3"))
+        document.getElementById("header__contacts-phones-list-id0").style.display = "none";
     }
 })
